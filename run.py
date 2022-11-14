3f0931f83c882465a82a7cf5379c942b472b3093
Samiksha <mailtosamiksha.s@gmail.com>
2022-11-14 14:10:35 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,73 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+"""
+The command to import random module for functions
+"""
+
+def main():
+    """
+    The main function invokes smaller functions and conditional statements
+
+    """
+
+    def player_name():
+        """
+        This functions inputs the name of the user as a string
+
+        """
+        username = input("Please enter your name here:")
+        return username
+
+
+    player = player_name()
+    print(f"Hi {player},How are you doing today 😊")
+    print("~~~~~WELCOME TO THE GAME OF R-P-S~~~~~")
+    print("""~~~~~WINNING RULES ARE:
+    1. PAPER VS ROCK --> PAPER WINS
+    2. ROCK VS SCISSORS --> ROCK WINS
+    3. SCISSORS VS PAPER --> SCISSORS WINS
+    """)
+
+
+    print(""" YOUR CHOICES ARE:
+    1. ROCK
+    2. PAPER
+    3. SCISSORS
+    """)
+
+    user_choice = int(input("Please select 1,2 or 3"))
+
+    print()
+
+    while user_choice < 1 or > 3:
+        """
+        Check if the user's choice is invalid or valid
+
+        """
+        print("Invalid response")
+        user_choice = int(input("Please select 1,2 or 3"))
+
+    if user_choice == 1:
+        choice = "ROCK" 
+    elif user_choice == 2:
+        choice = "PAPER"
+    else:
+        choice = "SCISSORS"
+
+    print (f"The user's choice is {choice}")  
+                 
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
