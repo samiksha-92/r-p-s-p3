d88f6978c6bd9e8caca8617b44e863dba3b97d65
Samiksha <mailtosamiksha.s@gmail.com>
2022-11-22 14:51:46 +0000

    5 starting functions written

---
+++ run.py
@@ -0,0 +1,55 @@
+import random
+"""
+The command to import random module for functions
+"""
+
+def player_name():
+    username = input("What's your name?")
+    return username
+
+player = player_name
+
+def starting_instructions():
+  print(f"Hi {player},How are you doing today 😊")
+  print("~~~~~ WELCOME TO THE GAME OF R-P-S ~~~~~")
+  print("""~~~~~ WINNING RULES ARE ~~~~~:
+  1. PAPER VS ROCK --> PAPER WINS
+  2. ROCK VS SCISSORS --> ROCK WINS
+  3. SCISSORS VS PAPER --> SCISSORS WINS
+    """)
+  print(""" YOUR CHOICES ARE:
+  1. ROCK
+  2. PAPER
+  3. SCISSORS
+  """)
+
+def player_selection():
+    player_choice = int(input("Please select 1,2 or 3:"))
+    return player_choice
+
+
+
+def determining_player_choice(player_choice):
+    if player_choice == 1:
+        choice = "ROCK"
+        print("You have chosen ROCK")
+    elif player_choice == 2:
+        choice = "PAPER"
+        print("You have chosen PAPER")
+    else:
+        choice = "SCISSORS"
+        print("You have chosen SCISSORS") 
+    return choice
+
+def comp_decision():
+    computer_choice = random.randint(1, 3)
+    return computer_choice
+    
+
+
+
+
+
+
+
+ 
